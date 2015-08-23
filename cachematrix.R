7bc8f0f493972c8c639d8dc9965a294f545c2414
perezv72 <perez.victor72@gmail.com>
2015-08-23 17:15:11 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,6 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
   if(!is.null(inv)) {
-    message("getting cached data.")
     return(inv)
   }
   data <- x$get()
